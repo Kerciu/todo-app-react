cd529f90d1c39dda73ecc990992c531949a48b09
Kerciu <kerciuuu@gmail.com>
2024-09-28 11:35:29 +0200

    created website layout, distributed sections evenly

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ const App = () => {
     <div>
       <header className='app-header'>Application Header</header>
       <main className='app-main'>
-        <section className='task-column'></section>
-        <section className='task-column'></section>
-        <section className='task-column'></section>
+        <section className='task-column'>Section 1</section>
+        <section className='task-column'>Section 2</section>
+        <section className='task-column'>Section 3</section>
       </main>
     </div>
   )

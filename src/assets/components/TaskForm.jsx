992fd572776fedd6462987ef47cc7bc5dfaa14ed
Kerciu <kerciuuu@gmail.com>
2024-09-28 12:06:33 +0200

    created styles for the tast status dropdown and task submit button

--- src/assets/components/TaskForm.jsx
+++ src/assets/components/TaskForm.jsx
@@ -8,13 +8,16 @@ const TaskForm = () => {
             <input type="text" className="task-input" placeholder="Enter your tags"/>
 
             <div className="task-form-bottom-line">
-                <select className="tast-status">
-                    <option value="todo">To do</option>
-                    <option value="doing">Doing</option>
-                    <option value="done">Done</option>
-                </select>
-
-                <button type="submit" className="task-submit"> + Add Task</button>
+                <div>
+                    <select className="task-status">
+                        <option value="todo">To do</option>
+                        <option value="doing">Doing</option>
+                        <option value="done">Done</option>
+                    </select>
+                </div>
+                <div>
+                    <button type="submit" className="task-submit"> + Add Task</button>
+                </div>
             </div>
         </form>
     </header>

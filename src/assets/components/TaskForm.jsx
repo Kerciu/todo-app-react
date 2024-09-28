057d872c1b57112499af5bc1e5c94b3823e5d2e8
Kerciu <kerciuuu@gmail.com>
2024-09-28 11:44:29 +0200

    added drop down presenting task status and add task button

--- src/assets/components/TaskForm.jsx
+++ src/assets/components/TaskForm.jsx
@@ -5,7 +5,15 @@ const TaskForm = () => {
         <form>
             <input type="text" className="task-input" placeholder="Enter your tags"/>
 
-            <div className="task-form-bottom-line"></div>
+            <div className="task-form-bottom-line">
+                <select className="tast-status">
+                    <option value="todo">To do</option>
+                    <option value="doing">Doing</option>
+                    <option value="done">Done</option>
+                </select>
+
+                <button type="submit" className="task-submit"> + Add Task</button>
+            </div>
         </form>
     </header>
 }

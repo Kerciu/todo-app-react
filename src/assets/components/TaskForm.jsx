9c4644400fdd3f71be8baccbbdf580ed84d386ce
Kerciu <Kerciuuu@gmail.com>
2024-09-30 10:31:19 +0200

    tracking states of task input box and task status dropdown using usestate

--- src/assets/components/TaskForm.jsx
+++ src/assets/components/TaskForm.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./TaskForm.css";
 
 const TaskForm = () => {
+
+    const [task, setTask] = useState("");
+    const [status, setStatus] = useState("To do");
+
+    const handleTaskChange = (e) => {
+        setTask(e.target.value)
+    };
+
+    const handleStatusChange = (e) => {
+        setStatus(e.target.value)
+    };
+
     return <header className="app-header">
         <form>
-            <input type="text" className="task-input" placeholder="Enter your task"/>
+            <input type="text" className="task-input" placeholder="Enter your task" 
+            onChange={handleTaskChange}/>
 
             <div className="task-form-bottom-line">
                 <div>
-                    <select className="task-status">
+                    <select className="task-status" onChange={handleStatusChange}>
                         <option value="todo">To do</option>
                         <option value="doing">Doing</option>
                         <option value="done">Done</option>
